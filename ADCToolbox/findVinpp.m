585b99fd17b28204e46d542f5f5fedf80b962688
many-question <jielu1995@outlook.com>
2025-08-25 16:48:28 +0800

    clean up developing files

--- ADCToolbox/findVinpp.m
+++
@@ -1,7 +0,0 @@
-function vpp_new = findVinpp(vpp_old, signal, signal_target)
-
-if nargin < 3
-    signal_target = -0.5;
-end
-
-vpp_new = vpp_old /  10^(signal/20) * 10^(signal_target/20);
--- TestFramework/path_setting.m
+++
@@ -1,8 +0,0 @@
-project_name = 'ZIC';
-script_path = '...\script';
-lib_path = '...\Instrument';            % download this from https://cloud.tsinghua.edu.cn/d/e09dc6d4cfa8418f8fee/
-data_path = '...\data';
-save_path = data_path;
-
-addpath(genpath(script_path));
-addpath(genpath(lib_path));
